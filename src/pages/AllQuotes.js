e68b23a8578c4710b3a77bb8925c4a7696789f52
Ali-ch88 <raza7861991@gmail.com>
2022-02-02 15:37:20 +0500

    commit

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,19 +1,10 @@
-import React, { Fragment } from "react";
-import { useParams, Route, Switch } from "react-router-dom";
-import Comments from "../components/comments/Comments";
+import React from "react";
 
 const AllQuotes = () => {
-  const params = useParams();
   return (
-    <Fragment>
+    <div>
       <h1>This is All Quote Page</h1>
-      <p>{params.quoteId}</p>
-
-      <Route path={`/quotes/${params.quoteId}/comments`}>
-        {/* <Comments /> */}
-        <h1>Ali</h1>
-      </Route>
-    </Fragment>
+    </div>
   );
 };
 
--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { useParams, Route, Switch } from "react-router-dom";
+import Comments from "../components/comments/Comments";
 
 const QuoteDetails = () => {
+  const params = useParams();
   return (
-    <div>
-      <h1>This is Quote details page</h1>
-    </div>
+    <Fragment>
+      <h1>This is All Quote Details Page</h1>
+      <p>{params.quoteId}</p>
+
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+        <h1>Ali</h1>
+      </Route>
+    </Fragment>
   );
 };
 
